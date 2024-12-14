ced7b3598960fb9ef19edf7a5b57e1591ddd3eb4
Frost-Core <coolularguy@gmail.com>
2024-12-14 16:56:26 +0000

    more muchpain fixes

--- st_stuff.c
+++ st_stuff.c
@@ -857,7 +857,7 @@ void ST_updateFaceWidget(void)
 	// getting hurt because of your own damn stupidity
 	if (plyr->damagecount)
 	{
-	    if (plyr->health - st_oldhealth > ST_MUCHPAIN)
+	    if (st_oldhealth - plyr->health > ST_MUCHPAIN)
 	    {
 		priority = 7;
 		st_facecount = ST_TURNCOUNT;
